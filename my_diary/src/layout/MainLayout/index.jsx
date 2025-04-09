f0a03e85a781ea69b2be6bdf572804acd2f54a98
hotteokButler <ygs12567@gmail.com>
2025-04-09 23:52:24 +0900

    fix : Mainlayout의 filter은 home 페이지일 때만 노출

--- my_diary/src/layout/MainLayout/index.jsx
+++ my_diary/src/layout/MainLayout/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useMatch } from 'react-router';
 
 import Filter from '../../components/Filter';
 import Header from '../../components/Header';
 import * as S from './mainLayout.styled';
 
 function MainLayout({ children, headerData }) {
+  const match = useMatch('/') || null;
+
   return (
     <S.MainWrapper>
       <S.MainCon>
         <Header header={headerData} />
-        <Filter />
+        {match && <Filter />}
 
         {children}
       </S.MainCon>
