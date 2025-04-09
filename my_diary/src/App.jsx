fb7e166d4bc3faa6a4b7fea22609fd900c07bfd4
hotteokButler <ygs12567@gmail.com>
2025-04-10 00:18:58 +0900

    일기 mock data

--- my_diary/src/App.jsx
+++ my_diary/src/App.jsx
@@ -1,3 +1,4 @@
+import { useReducer } from 'react';
 import { Route, Routes } from 'react-router';
 
 import Diary from './pages/Diary';
@@ -6,7 +7,19 @@ import Home from './pages/Home';
 import NewDiary from './pages/NewDiary';
 import NotFound from './pages/NotFound';
 
+const mockData = [
+  { id: 1, emotinoID: 1, createdDate: new Date().getTime(), content: '1번 일기 내용' },
+  { id: 2, emotinoID: 2, createdDate: new Date().getTime(), content: '2번 일기 내용' },
+  { id: 3, emotinoID: 3, createdDate: new Date().getTime(), content: '3번 일기 내용' },
+];
+
+const reducer = (state, action) => {
+  return state;
+};
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
--- my_diary/src/pages/Edit/index.jsx
+++ my_diary/src/pages/Edit/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 import MainLayout from '../../layout/MainLayout';
 
 function Eidt() {
-  return <MainLayout>edit</MainLayout>;
+  const params = useParams();
+
+  return <MainLayout>{params.id}번 일기 수정페이지입니다.</MainLayout>;
 }
 
 export default Eidt;
