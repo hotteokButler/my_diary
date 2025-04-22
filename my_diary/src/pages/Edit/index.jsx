caa28afa6c52c7f70acb43623b2b364e862983d6
hotteokButler <ygs12567@gmail.com>
2025-04-23 00:39:42 +0900

    삭제 확인 추가

--- my_diary/src/pages/Edit/index.jsx
+++ my_diary/src/pages/Edit/index.jsx
@@ -11,6 +11,18 @@ function Eidt() {
   const navigate = useNavigate();
   const dispatchFn = useContext(DiaryDispatchContext);
 
+  let isDelete = false;
+
+  const onClickDelete = () => {
+    isDelete = window.confirm('일기를 정말 삭제할까요?\n다시 복구되지 않습니다.');
+    if (isDelete) {
+      dispatchFn.onDelete(params.id);
+      navigate(-1, { replace: true });
+    } else {
+      return;
+    }
+  };
+
   const headerData = {
     title: '일기 수정하기',
     leftChildFn: () => navigate(-1),
@@ -22,8 +34,7 @@ function Eidt() {
     rightChild: '삭제하기',
     rightType: 'NEGATIVE',
     rightChildFn: () => {
-      dispatchFn.onDelete(params.id);
-      navigate(-1, { replace: true });
+      onClickDelete();
     },
   };
 
